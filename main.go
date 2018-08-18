5a1862961f13f2dafa5440e0a0280933a25d5d47
Volker Schukai <volker.schukai@schukai.de>
2018-08-18 15:56:06 +0200

    Images

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	// Allocate two-dimensioanl array.
+	a := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		a[i] = make([]uint8, dx)
+	}
+	
+	// Do something.
+	for i := 0; i < dy; i++ {
+		for j := 0; j < dx; j++ {
+			a[i][j] = uint8( (i^j)-(i+j))
+		}
+	}	
+	return a
+}
 
 func main() {
-    fmt.Println("Hello, world!")
+	pic.Show(Pic)
 }
